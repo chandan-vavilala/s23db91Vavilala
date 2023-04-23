3a7d92b438b451be68fe87d1fe026308e8131de1
chandan-vavilala <chandanreddyvavilala@gmail.com>
2023-04-23 17:40:13 -0500

    Update app.js

--- app.js
+++ app.js
@@ -6,20 +6,7 @@ var logger = require('morgan');
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 
-passport.use(new LocalStrategy(
-  function(username, password, done) {
-  Account.findOne({ username: username }, function (err, user) {
-  if (err) { return done(err); }
-  if (!user) {
-  return done(null, false, { message: 'Incorrect username.' });
-  }
-  if (!user.validPassword(password)) {
-  return done(null, false, { message: 'Incorrect password.' });
-  }
-  return done(null, user);
-  });
-  }
-))
+
 
 require('dotenv').config();
 const connectionString =
